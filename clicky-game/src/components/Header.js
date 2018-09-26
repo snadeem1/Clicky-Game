2ebda66144d246db50580060b8295dfcb8c0b4bb
Sitwat <sitwat.nadeem1@gmail.com>
2018-09-26 17:06:55 -0400

    react files

---
+++ clicky-game/src/components/Header.js
@@ -0,0 +1,16 @@
+import Reach from 'react';
+
+
+const Header = props => (
+<div className= "header">
+<div className= "title">{props.children}</div>
+<div className = "scores">
+
+</div>
+
+</div>
+);
+
+
+
+export default Header;
