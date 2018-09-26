3fb0c62d7985fbd6eb79da9433be96df08d368c2
Sitwat <sitwat.nadeem1@gmail.com>
2018-09-26 17:41:21 -0400

    components

--- clicky-game/src/components/Header.js
+++ clicky-game/src/components/Header.js
@@ -1,10 +1,11 @@
-import Reach from 'react';
-
+import React from 'react';
 
 const Header = props => (
 <div className= "header">
 <div className= "title">{props.children}</div>
 <div className = "scores">
+Score: {props.score}
+Top Score: {props.TopScore}
 
 </div>
 
